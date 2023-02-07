06faf7651547dce921ba2721e4caa7de074c3308
Marina <111200416+marina-ohrumchyk@users.noreply.github.com>
2023-02-08 00:25:05 +0200

    01color commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick() {
+  timerId = setInterval(getRandomHexColor, 1000);
+  startButton.setAttribute('disabled', true);
+}
+
+function onStopButtonClick() {
+  startButton.removeAttribute('disabled');
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`;
+}
