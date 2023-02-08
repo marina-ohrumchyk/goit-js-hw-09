4a9df2aa8c617b150d11d1fd044d1bbc234b4a40
Marina <111200416+marina-ohrumchyk@users.noreply.github.com>
2023-02-08 15:25:29 +0200

    03promises commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+const btnElement = document.querySelector('button');
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+btnElement.addEventListener('click', onBtn);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onBtn(evt) {
+  evt.preventDefault();
+  for (let position = 1; position <= amount.value; position += 1) {
+    createPromise(position, delay.value)
+      .then(({ position, delay }) => {
+        console.log(
+          `:белая_галочка: Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        console.log(`:х: Rejected promise ${position} in ${delay}ms`);
+      });
+    delay.value += step.value;
   }
 }
